099e3f4d3d69152d88f3512f2d8c58c8ec261cbe
Marcus Noble <github@marcusnoble.co.uk>
2016-03-03 18:54:51 +0000

    Added custom image support

--- app.js
+++ app.js
@@ -1,77 +1,43 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var fs = require('fs');
-var svgexport = require('svgexport');
-var wrap = require('wordwrap')(30);
-var Imagemin = require('imagemin');
+'use strict';
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const fs = require('fs');
+const svgexport = require('svgexport');
+const wrap = require('wordwrap')(30);
+const Imagemin = require('imagemin');
+const request = require('request').defaults({ encoding: null });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var people = {
-  'ben' : {
-    'file': 'images/ben.png',
-    'x': 340,
-    'y': 80,
-    'fontSize': 25,
-    'rotation': 0
-  },
-  'seren' : {
-    'file': 'images/seren.png',
-    'x': 300,
-    'y': 100,
-    'fontSize': 30,
-    'rotation': 0
-  },
-  'ryan' : {
-    'file': 'images/ryan.png',
-    'x': 20,
-    'y': 100,
-    'fontSize': 20,
-    'rotation': 0
-  },
-  'ruth' : {
-    'file': 'images/ruth.png',
-    'x': 120,
-    'y': 120,
-    'fontSize': 20,
-    'rotation': 0
-  },
-  'pete' : {
-    'file': 'images/pete.png',
-    'x': 60,
-    'y': 100,
-    'fontSize': 30,
-    'rotation': 2
-  }
-}
+const people = require('./people');
 
 // Base64 images on start
-for(var person in people) {
+for(let person in people) {
   if(people.hasOwnProperty(person)) {
-    var baseImage = fs.readFileSync(people[person].file);
+    let baseImage = fs.readFileSync(people[person].file);
     people[person].file = 'data:image;base64,' + baseImage.toString('base64');
   }
 }
 
 function generateImage(timestamp, person, text, done) {
-  var formattedText = '';
-  wrap(text).split('\n').forEach(function(line) {
-    formattedText += `<tspan x="${people[person].x}" dy="25">${line}</tspan>`
+  let formattedText = '';
+  wrap(text).split('\n').forEach((line) => {
+    formattedText += `<tspan x="${person.x}" dy="25">${line}</tspan>`
   });
 
-  var svg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  let svg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
     <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="743px" height="418px" viewbox="0 0 743 418">
       <defs>
         <pattern id="img1" width="100%" height="100%">
-          <image xlink:href="${people[person].file}" x="0" y="0" width="100%" height="100%" />
+          <image xlink:href="${person.file}" x="0" y="0" width="100%" height="100%" />
         </pattern>
       </defs>
       <path d="M0 0 H 743 V 418 H 0 L 0 0" fill="url(#img1)"/>
-      <text x="${people[person].x}" y="${people[person].y}" transform="rotate(${people[person].rotation} ${people[person].x} ${people[person].y})" font-family="Arial" font-size="${people[person].fontSize}">
+      <text x="${person.x}" y="${person.y}" transform="rotate(${person.rotation} ${person.x} ${person.y})" font-family="Arial" font-size="${person.fontSize}">
           ${formattedText}
       </text>
     </svg>`;
@@ -81,8 +47,7 @@ function generateImage(timestamp, person, text, done) {
   svgexport.render({
       'input' : `${timestamp}.svg`,
       'output' : `${timestamp}.png`
-    }, function(err) {
-
+    }, (err) => {
       new Imagemin()
         .src(`${timestamp}.png`)
         .dest(`./`)
@@ -93,29 +58,57 @@ function generateImage(timestamp, person, text, done) {
     });
 }
 
+app.get('/custom', function(req, res) {
+  const timestamp = Date.now();
+  request.get(req.query.image, function (err, response, body) {
+    if (!err && response.statusCode == 200) {
+        let base64Image = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+        let customPerson = {
+          file: base64Image,
+          x: req.query.x,
+          y: req.query.y,
+          rotation: req.query.rotation || 0,
+          fontSize: req.query.fontSize || 25
+        };
+
+        generateImage(timestamp, customPerson, req.query.message, function(err) {
+          if(err) return err;
+
+          let returnImg = fs.readFileSync(`${timestamp}.png`);
+          res.writeHead(200, {'Content-Type': 'image/png' });
+          res.end(returnImg, 'binary');
+
+          fs.unlink(`${timestamp}.svg`);
+          fs.unlink(`${timestamp}.png`);
+          return;
+        });
+    }
+  });
+});
+
 app.get('/:person/:text', function (req, res) {
-  var timestamp = Date.now();
+  const timestamp = Date.now();
 
   if(!people[req.params.person]) {
     return res.send('Unable to find person');
   }
 
-  generateImage(timestamp, req.params.person, req.params.text, function(err) {
+  generateImage(timestamp, people[req.params.person], req.params.text, function(err) {
     if(err) return err;
 
-    var returnImg = fs.readFileSync(`${timestamp}.png`);
+    let returnImg = fs.readFileSync(`${timestamp}.png`);
     res.writeHead(200, {'Content-Type': 'image/png' });
     res.end(returnImg, 'binary');
 
     fs.unlink(`${timestamp}.svg`);
     fs.unlink(`${timestamp}.png`);
     return;
-  })
+  });
 });
 
 app.get('*', function(req, res) {
   res.render('index', {people: people});
-})
+});
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
---
+++ people.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'ben' : {
+    'file': 'images/ben.png',
+    'x': 340,
+    'y': 80,
+    'fontSize': 25,
+    'rotation': 0
+  },
+  'seren' : {
+    'file': 'images/seren.png',
+    'x': 300,
+    'y': 100,
+    'fontSize': 30,
+    'rotation': 0
+  },
+  'ryan' : {
+    'file': 'images/ryan.png',
+    'x': 20,
+    'y': 100,
+    'fontSize': 20,
+    'rotation': 0
+  },
+  'ruth' : {
+    'file': 'images/ruth.png',
+    'x': 120,
+    'y': 120,
+    'fontSize': 20,
+    'rotation': 0
+  },
+  'pete' : {
+    'file': 'images/pete.png',
+    'x': 60,
+    'y': 100,
+    'fontSize': 30,
+    'rotation': 2
+  }
+};
