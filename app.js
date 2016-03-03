2093f262923eab36093641315d573ca40e98834e
Marcus Noble <github@marcusnoble.co.uk>
2016-03-03 06:30:08 +0000

    Added overview page

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var express = require('express');
-var app = express();
+var exphbs  = require('express-handlebars');
 var fs = require('fs');
 var svgexport = require('svgexport');
 var wrap = require('wordwrap')(30);
 
 var port = process.env.PORT || 3000;
 
+var app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 var people = {
   'ben' : {
-    'file': 'ben.png',
+    'file': 'images/ben.png',
     'x': 340,
     'y': 80,
     'fontSize': 25,
     'rotation': 0
+  }
+}
+
+// Base64 images on start
+for(var person in people) {
+  if(people.hasOwnProperty(person)) {
+    var baseImage = fs.readFileSync(people[person].file);
+    people[person].file = 'data:image;base64,' + baseImage.toString('base64');
   }
 }
 
@@ -22,13 +34,11 @@ function generateImage(timestamp, person, text, done) {
     formattedText += `<tspan x="${people[person].x}" dy="25">${line}</tspan>`
   });
 
-  var baseImage = fs.readFileSync(people[person].file);
-  var base64Image = baseImage.toString('base64');
   var svg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
     <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="743px" height="418px" viewbox="0 0 743 418">
       <defs>
         <pattern id="img1" width="100%" height="100%">
-          <image xlink:href="data:image;base64,${base64Image}" x="0" y="0" width="100%" height="100%" />
+          <image xlink:href="${people[person].file}" x="0" y="0" width="100%" height="100%" />
         </pattern>
       </defs>
       <path d="M0 0 H 743 V 418 H 0 L 0 0" fill="url(#img1)"/>
@@ -67,6 +77,10 @@ app.get('/:person/:text', function (req, res) {
     return;
   })
 });
+
+app.get('*', function(req, res) {
+  res.render('index', {people: people});
+})
 
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
